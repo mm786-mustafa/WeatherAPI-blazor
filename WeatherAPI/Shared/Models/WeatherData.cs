70c4f3fd052961176ff4cf87632de022565dd2df
Muhammad Mustafa <115900395+mm786-mustafa@users.noreply.github.com>
2024-05-13 21:27:11 +0500

    Initial commit

---
+++ WeatherAPI/Shared/Models/WeatherData.cs
@@ -0,0 +1,22 @@
+﻿namespace WeatherAPI.Shared.Models
+{
+    public class WeatherData
+    {
+        public string? Name { get; set; }
+        public MainData Main { get; set; }
+        public WeatherInfo[] Weather { get; set; }
+    }
+
+    public class MainData
+    {
+        public float Temp { get; set; }
+        public float Feels_like { get; set; }
+        public int Humidity { get; set; }
+    }
+
+    public class WeatherInfo
+    {
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+}
---
+++ WeatherAPI/Shared/Service/WeatherService.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Json;
+using WeatherAPI.Shared.Models;
+
+namespace WeatherAPI.Shared.Service
+{
+    public class WeatherService
+    {
+        private readonly HttpClient _httpClient;
+
+        public WeatherService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<WeatherData> GetWeather(string cityName)
+        {
+            return await _httpClient.GetFromJsonAsync<WeatherData>("https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&units=metric&appid=f0c42ff3a38a1ff235b42553bb662aab");
+        }
+    }
+}
